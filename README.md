d3769ce17cd0c3e5c73b6c902313d9725c10e6e8
vikash1993 <vikash.vnsit1@gmail.com>
2017-10-28 23:36:58 +0530

    Angular2 CRUD operation with message bootstrap

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+echo "# Angular2CRUD" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/vikash1993/Angular2CRUD.git
+git push -u origin master
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular 2 CURD Operation with an Array';
+  employees = [
+    {name: "sing", position: 'programer'},
+    {name: "jone", position: 'Manager'},
+    {name: "Maria", position: 'tester'}    
+  ];
+  model: any={};
+  model2: any={};
+  msg:any = "";
+  addEmployee(){
+    this.employees.push(this.model);
+    this.model = {};
+    this.msg ="save data";
+  }
+
+  DeleteEmployee(i){
+    this.employees.splice(i, 1);
+    console.log(i);
+    this.msg ="Data Deleted";    
+  }
+  myValue;
+  editEmployee(k){
+    this.model2.name = this.employees[k].name;
+    this.model2.position = this.employees[k].position;
+    this.myValue = k;
+  }
+  updateEmployee(){
+    let k=this.myValue;
+    for(let i=0; i<this.employees.length; i++){
+      if(i==k){
+        this.employees[i] = this.model2;
+        this.model2 = {};
+        this.msg ="update data";    
+      }
+    }
+  }
+  clickMe(){
+    this.msg = "";
+  }
 }
